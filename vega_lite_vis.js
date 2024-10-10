0d6cbc4cf620cedc5828889075ba72369cebfff8
Jasmine Koh <jkoh0034@student.monash.edu>
2024-10-10 22:03:51 +0800

    update location of files

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,7 +1,7 @@
-var vg_1 = "/AS2/visualisation/drug-addicts-in-malaysia.vg.json";
-var vg_2 = "/AS2/visualisation/drug-types.vg.json";
-var vg_3 = "/AS2/visualisation/drug-age.vg.json";
-var vg_4 = "/AS2/visualisation/drug-sex.vg.json";
+var vg_1 = "drug-addicts-in-malaysia.vg.json";
+var vg_2 = "drug-types.vg.json";
+var vg_3 = "drug-age.vg.json";
+var vg_4 = "drug-sex.vg.json";
 
 vegaEmbed('#choropleth_map', vg_1).then(function(result) {}).catch(console.error);
 vegaEmbed('#drug_types_chart', vg_2).then(function(result) {}).catch(console.error);
